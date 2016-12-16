7b3b19783c69a74fe37d48ac3a3f98e8df650eb5
Smbat Tumasyan <smbatt@eduardm-318.tce.loc>
2016-12-16 12:01:27 +0400

    Initial Commit

---
+++ WebServiceCL/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WebServiceCL
+//
+//  Created by Smbat Tumasyan on 16.12.16.
+//  Copyright © 2016 EGS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
